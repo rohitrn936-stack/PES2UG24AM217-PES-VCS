c73de5cc2b18984f3223b28fde51bf56cc850947
Rohit <rohitrn936@gmail.com>
2026-04-16 11:48:33 +0000

    test code

--- tree.c
+++ tree.c
@@ -12,6 +12,15 @@
 #define MODE_EXEC      0100755
 #define MODE_DIR       0040000
 
+uint32_t get_file_mode(const char *path) {
+    struct stat st;
+    if (lstat(path, &st) != 0) return 0;
+
+    if (S_ISDIR(st.st_mode))  return MODE_DIR;
+    if (st.st_mode & S_IXUSR) return MODE_EXEC;
+    return MODE_FILE;
+}
+
 int tree_parse(const void *data, size_t len, Tree *tree_out) {
     tree_out->count = 0;
     const uint8_t *ptr = (const uint8_t *)data;
